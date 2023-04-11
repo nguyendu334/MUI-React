71194e1ddff4433efdb8da80f3a8cce2cc2a712c
_nguyen.du <1851061329@e.tlu.edu.vn>
2023-04-11 14:15:26 +0700

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { Button, TextField, Typography } from '@mui/material';
+import { useState, useEffect } from 'react';
+import { Button, Typography, AppBar, Toolbar, Tabs, Tab } from '@mui/material';
 import Form from './components/Form';
+
 function App() {
+    const [value, setValue] = useState(0);
     return (
         <div>
-            <Typography variant="h2" sx={{ color: 'red' }}>
+            {/* <Typography variant="h2" sx={{ color: 'red' }}>
                 MUI
             </Typography>
             <p>sdasdaasdasdasd</p>
@@ -18,7 +21,28 @@ function App() {
             <br />
             <br />
             <br />
-            <Form />
+            <Form /> */}
+
+            <AppBar position="static">
+                <Toolbar>
+                    <img src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/3209893/material-ui-icon-md.png" alt="logo" height="50px" />
+                    <Typography variant="h5" sx={{marginLeft: '15px'}}>MUI</Typography>
+                    <Tabs
+                        sx={{ marginLeft: 'auto' }}
+                        value={value}
+                        onChange={(e, newValue) => setValue(newValue)}
+                        textColor="inherit"
+                        indicatorColor="secondary"
+                    >
+                        <Tab label="Home" />
+                        <Tab label="About" />
+                        <Tab label="Contact" />
+                    </Tabs>
+                    <Button variant="contained" color="success" sx={{ marginLeft: 'auto' }}>
+                        Signin
+                    </Button>
+                </Toolbar>
+            </AppBar>
         </div>
     );
 }
