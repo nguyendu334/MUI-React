fd4d51ed9bb14914a5355ceef2d2f94537837d6f
_nguyen.du <1851061329@e.tlu.edu.vn>
2023-04-11 10:27:01 +0700

    Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 function App() {
     return (
         <div>
             <Typography variant="h2" sx={{color: 'red'}}>MUI</Typography>
             <p>sdasdaasdasdasd</p>
-        
+            <Button>Click me</Button>
+            <Button variant='contained'  color='success' sx={{margin: '30px'}}>Click me</Button>
+            <Button variant='outlined' onClick={() => alert('hello')}>Click me</Button>
         </div>
     );
 }
